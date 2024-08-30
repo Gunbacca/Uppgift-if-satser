5000d02fda937eb9b0385279437d87ac7eb2a7f5
Gunbacca <albe07001@utb.vaxjo.se>
2024-08-30 13:55:13 +0200

    ja och nej
    ja och nej om det är fint väder

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http.Headers;
+
+{
+Console.WriteLine("är det fint väder");
+string svar = Console.ReadLine();
+if(svar == "j" || svar == "J")
+Console.Write("Vi går på picknick");
+else if(svar == "n" || svar == "N")
+Console.Write("vi stannar inne och spelar dator");
+else
+Console.WriteLine("jag förstår inte");
+}
+
